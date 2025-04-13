2b9ca2d78056381da159362e9d2788717870509a
zagorky <zagorkiynahyi@gmail.com>
2025-04-13 20:28:36 +0300

    feat(main page): add redirect to auth page

--- fun-chat/src/routes/main.tsx
+++ fun-chat/src/routes/main.tsx
@@ -2,8 +2,19 @@ import { Form } from '../components/form/form.tsx';
 import { Header } from '../components/header/header.tsx';
 import { Footer } from '../components/footer/footer.tsx';
 import { Header1 } from '../components/headers/headers.tsx';
+import { useEffect } from 'react';
+import { useNavigate } from '@tanstack/react-router';
+import { useAuthStore } from '../store/use-auth-store.ts';
 
 export default function MainPage() {
+  const navigate = useNavigate();
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate({ to: '/login' }).catch((error) => console.log(error));
+    }
+  }, [isAuthenticated, navigate]);
   return (
     <>
       <Header />
