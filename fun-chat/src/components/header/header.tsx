7d4e5cc9aa8b3a82d6f38cf2ee2beabe7d51ae93
zagorky <zagorkiynahyi@gmail.com>
2025-04-13 20:45:44 +0300

    feat(header): add buttons state depends on the auth

--- fun-chat/src/components/header/header.tsx
+++ fun-chat/src/components/header/header.tsx
@@ -1,26 +1,30 @@
 import { Button } from '../button/button.tsx';
 import { Header1 } from '../headers/headers.tsx';
 import { Link } from '@tanstack/react-router';
+import { useAuthStore } from '../../store/use-auth-store.ts';
 
 export function Header() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
   const style = ['flex', 'items-center', 'w-full', 'justify-between', 'p-4'];
   return (
     <header className={[...style].join(' ')}>
       <Button type={'button'}>
         <Link to="/about">About Page</Link>
       </Button>
-      <Button type={'button'}>
+      <Button type={'button'} disabled={isAuthenticated}>
         <Link to="/login">Login page</Link>
       </Button>
       <Header1 title={'Popik Chat'} />
 
-      <Button type={'button'}>
+      <Button type={'button'} disabled={!isAuthenticated}>
         <Link to="/main">Chat Page</Link>
       </Button>
       <Button
         onClick={() => {
           console.log(' Header heh');
         }}
+        disabled={!isAuthenticated}
       >
         Exit
       </Button>
