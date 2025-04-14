a12faebcee81ea0003d70d8dd00e9af7767ec880
zagorky <zagorkiynahyi@gmail.com>
2025-04-14 19:23:18 +0300

    feat(auth-store): add saving to ss

--- fun-chat/src/store/use-auth-store.ts
+++ fun-chat/src/store/use-auth-store.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { validateLogin, validatePassword } from '../utils/utilities.ts';
 import { sendWebSocketMessage } from '../socket.ts';
 import type { ServerResponse } from '../types/types.ts';
+import { persist, devtools, createJSONStorage } from 'zustand/middleware';
 
 type AuthStore = {
   login: string;
@@ -12,6 +13,7 @@ type AuthStore = {
   };
   authError: string;
   isAuthenticated: boolean;
+  sessionToken: string;
 
   setLogin: (login: string) => void;
   setPassword: (password: string) => void;
@@ -23,97 +25,127 @@ type AuthStore = {
   logout: () => void;
 };
 
-export const useAuthStore = create<AuthStore>((set, get) => ({
-  login: '',
-  password: '',
-  errors: {
-    login: '',
-    password: '',
-  },
-  authError: '',
-  isAuthenticated: false,
-
-  setLogin: (login) =>
-    set((state) => ({
-      login,
-      errors: {
-        ...state.errors,
-        login: validateLogin(login),
-      },
-    })),
-
-  setPassword: (password) =>
-    set((state) => ({
-      password,
-      errors: {
-        ...state.errors,
-        password: validatePassword(password),
-      },
-    })),
-
-  setErrors: (errors) => set({ errors }),
-
-  loginSuccess: () => set({ isAuthenticated: true, authError: '' }),
-
-  loginFailure: (error) => set({ authError: error, isAuthenticated: false }),
-
-  validateAndSubmit: (event) => {
-    event.preventDefault();
-    const { login, password } = get();
-
-    const loginError = validateLogin(login);
-    const passwordError = validatePassword(password);
-
-    if (loginError || passwordError) {
-      set({
-        errors: {
-          login: loginError,
-          password: passwordError,
-        },
-      });
-      return;
-    }
-
-    sendWebSocketMessage({
-      id: crypto.randomUUID(),
-      type: 'USER_LOGIN',
-      payload: {
-        user: {
-          login: login,
-          password: password,
-        },
-      },
-    });
-  },
-  clearAuthError: () => set({ authError: '' }),
-  logout: () => {
-    const currentState = get();
-
-    set({
-      login: '',
-      password: '',
-      errors: {
+export const useAuthStore = create<AuthStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
         login: '',
         password: '',
-      },
-      authError: '',
-      isAuthenticated: false,
-    });
-
-    if (currentState.isAuthenticated) {
-      sendWebSocketMessage({
-        id: crypto.randomUUID(),
-        type: 'USER_LOGOUT',
-        payload: {
-          user: {
-            login: currentState.login,
-            password: currentState.password,
-          },
+        errors: {
+          login: '',
+          password: '',
         },
-      });
-    }
-  },
-}));
+        authError: '',
+        isAuthenticated: false,
+        sessionToken: '',
+
+        setLogin: (login) =>
+          set(
+            (state) => ({
+              login,
+              errors: {
+                ...state.errors,
+                login: validateLogin(login),
+              },
+            }),
+            false,
+            'setLogin',
+          ),
+
+        setPassword: (password) =>
+          set(
+            (state) => ({
+              password,
+              errors: {
+                ...state.errors,
+                password: validatePassword(password),
+              },
+            }),
+            false,
+            'setPassword',
+          ),
+
+        setErrors: (errors) => set({ errors }, false, 'setErrors'),
+
+        loginSuccess: () => set({ isAuthenticated: true, authError: '' }, false, 'loginSuccess'),
+
+        loginFailure: (error) =>
+          set({ authError: error, isAuthenticated: false }, false, 'loginFailure'),
+
+        validateAndSubmit: (event) => {
+          event.preventDefault();
+          const { login, password } = get();
+
+          const loginError = validateLogin(login);
+          const passwordError = validatePassword(password);
+
+          if (loginError || passwordError) {
+            set(
+              {
+                errors: {
+                  login: loginError,
+                  password: passwordError,
+                },
+              },
+              false,
+              'validateAndSubmit/error',
+            );
+            return;
+          }
+
+          sendWebSocketMessage({
+            id: crypto.randomUUID(),
+            type: 'USER_LOGIN',
+            payload: {
+              user: {
+                login: login,
+                password: password,
+              },
+            },
+          });
+        },
+
+        clearAuthError: () => set({ authError: '' }, false, 'clearAuthError'),
+
+        logout: () => {
+          const currentState = get();
+
+          set(
+            {
+              login: '',
+              password: '',
+              errors: {
+                login: '',
+                password: '',
+              },
+              authError: '',
+              isAuthenticated: false,
+            },
+            false,
+            'logout/reset',
+          );
+
+          if (currentState.isAuthenticated) {
+            sendWebSocketMessage({
+              id: crypto.randomUUID(),
+              type: 'USER_LOGOUT',
+              payload: {
+                user: {
+                  login: currentState.login,
+                  password: currentState.password,
+                },
+              },
+            });
+          }
+        },
+      }),
+      {
+        name: 'AuthStore',
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+  ),
+);
 
 export const handleServerMessage = (data: ServerResponse) => {
   if (data.type === 'USER_LOGIN') {
