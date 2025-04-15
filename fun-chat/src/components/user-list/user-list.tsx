89cc9c690718263dc3895ec362f61d42045b87eb
zagorky <zagorkiynahyi@gmail.com>
2025-04-15 14:02:43 +0300

    feat(user-list): add search

--- fun-chat/src/components/user-list/user-list.tsx
+++ fun-chat/src/components/user-list/user-list.tsx
@@ -1,27 +1,49 @@
 import { useChatStore } from '../../store/use-chat-store.ts';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthStore } from '../../store/use-auth-store.ts';
+import { Input } from '../input/input.tsx';
 
 export function UserList() {
   const getUsers = useChatStore((state) => state.getUsers);
   const users = useChatStore((state) => state.users);
   const error = useChatStore((state) => state.error);
   const currentUser = useAuthStore((state) => state.login);
+  const [searchParameter, setSearchParameter] = useState('');
 
   useEffect(() => {
     getUsers();
   }, [getUsers]);
 
+  const filteredUsers = users.filter((user) => {
+    if (user.login === currentUser || !user.isLogined) {
+      return false;
+    }
+    if (!searchParameter) {
+      return true;
+    }
+    return user.login.toLowerCase().includes(searchParameter.toLowerCase());
+  });
+
   if (error) {
     return <div>Error: {error}</div>;
   }
   return (
-    <section className="border border-amber-600 opacity-80 rounded-lg shadow-sm p-4 m-1 min-h-32">
+    <section className="border border-b-pink-900 opacity-80 rounded-lg shadow-sm p-4 m-1 min-h-32 overflow-y-auto">
       <h3 className="border-b-2 text-teal-900 font-bold">Online users:</h3>
+      <Input
+        id="search"
+        label=""
+        value={searchParameter}
+        onChange={(event) => setSearchParameter(event.target.value)}
+        type="text"
+        placeholder="Search..."
+      />
       <ul>
-        {users.map((user) =>
-          user.login === currentUser ? null : <li key={user.login}>😉{user.login}</li>,
-        )}
+        {filteredUsers.map((user) => (
+          <li key={user.login}>
+            😉{user.login} - {user.isLogined}
+          </li>
+        ))}
       </ul>
     </section>
   );
