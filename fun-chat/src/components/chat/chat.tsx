10c3f7d7f791c70be117a324b5b3b4244b6cfadc
zagorky <zagorkiynahyi@gmail.com>
2025-04-16 09:20:39 +0300

    feat(chat): add user status

--- fun-chat/src/components/chat/chat.tsx
+++ fun-chat/src/components/chat/chat.tsx
@@ -7,7 +7,8 @@ export function Chat() {
   return (
     <section className="border border-amber-600 opacity-80 rounded-lg shadow-sm p-4 m-1 flex flex-col max-h-[70vh]">
       <h3 className="border-b-2 text-teal-900 font-bold">
-        Conversation: {selectedUser ? selectedUser.login : ''}
+        Conversation: {selectedUser?.isLogined ? '😉' : '🫥'}
+        {selectedUser ? selectedUser.login : ''}{' '}
       </h3>
       {selectedUser ? <div>Messages</div> : <p>Select a user to start chatting</p>}
       <ChatForm />
