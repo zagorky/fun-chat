bbf29aac12cbe20b6cc5668ea72b883055423bf9
zagorky <zagorkiynahyi@gmail.com>
2025-04-17 20:36:41 +0300

    feat(chat-dialog): add scroll to new message

--- fun-chat/src/components/chat-dialog/chat-dialog.tsx
+++ fun-chat/src/components/chat-dialog/chat-dialog.tsx
@@ -1,17 +1,23 @@
 import { Message } from '../message/message.tsx';
 import { useChatStore } from '../../stores/use-chat-store.ts';
 import { useAuthStore } from '../../stores/use-auth-store.ts';
+import { useEffect, useRef } from 'react';
 
 export function ChatDialog() {
   const selectedUser = useChatStore((state) => state.selectedUser);
   const messages = useChatStore((state) => state.messages);
   const currentUser = useAuthStore((state) => state.login);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
   const filteredMessages = messages.filter(
     (message) =>
       (message.from === currentUser && message.to === selectedUser?.login) ||
       (message.from === selectedUser?.login && message.to === currentUser),
   );
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [filteredMessages]);
 
   return (
     <ul className="mt-2 pr-2 space-y-1 overflow-y-auto max-h-[calc(70vh-100px)] flex-grow">
@@ -34,6 +40,7 @@ export function ChatDialog() {
       ) : (
         <p>Select a user to start chatting</p>
       )}
+      <div ref={messagesEndRef}></div>
     </ul>
   );
 }
