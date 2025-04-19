c519820b73051592575f3c3f91e41a5310078199
zagorky <zagorkiynahyi@gmail.com>
2025-04-19 09:33:48 +0300

    refactor(chat): rewrite condition

--- fun-chat/src/components/chat/chat.tsx
+++ fun-chat/src/components/chat/chat.tsx
@@ -8,8 +8,12 @@ export function Chat() {
   return (
     <section className="border border-amber-600 opacity-80 rounded-lg shadow-sm p-4 m-1 flex flex-col max-h-[70vh] min-h-[70vh] max-[750px]:max-h-[48vh] max-[750px]:min-h-[48vh]">
       <h3 className="border-b-2 text-teal-900 font-bold">
-        Conversation: {selectedUser?.isLogined ? '😉' : '🫥'}
-        {selectedUser ? selectedUser.login : ''}
+        Conversation:{' '}
+        {selectedUser && (
+          <>
+            {selectedUser.isLogined ? <>{selectedUser.login} 😉</> : <>{selectedUser.login} 🫥</>}
+          </>
+        )}
       </h3>
       <ChatDialog />
       <ChatForm />
