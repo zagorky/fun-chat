2e297ae0739195b98663528e27ffda5d22fe720f
zagorky <zagorkiynahyi@gmail.com>
2025-04-21 16:10:38 +0300

    refactor: add stylistic changes

--- fun-chat/src/api/utils/authorization.ts
+++ fun-chat/src/api/utils/authorization.ts
@@ -1,6 +1,7 @@
-import { useAuthStore } from '../stores/use-auth-store.ts';
+import { useAuthStore } from '../../stores/use-auth-store.ts';
 import { sendWebSocketMessage } from '../socket.ts';
-import { useChatStore } from '../stores/use-chat-store.ts';
+import { useChatStore } from '../../stores/use-chat-store.ts';
+import type React from 'react';
 
 export function validateLogin(login: string) {
   const MIN_LENGTH = 4;
@@ -66,9 +67,9 @@ export const sendLogoutMessage = (login: string, password: string) => {
 export const handleLogout = () => {
   const { login, password, isAuthenticated, logout } = useAuthStore.getState();
   useChatStore.setState({ selectedUser: null });
-  logout();
 
   if (isAuthenticated && login && password) {
     sendLogoutMessage(login, password);
+    logout();
   }
 };
